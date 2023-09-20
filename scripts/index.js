385db00eb23dd7a56e7dbc43a3c55612f8273183
unknown <adrianfernadezrodriguez@alumnos.tuniversformacion.es>
2023-09-20 09:21:11 +0200

    Inserccion

---
+++ scripts/index.js
@@ -0,0 +1,17 @@
+
+// Función para generar un color aleatorio en formato hexadecimal
+const generateRamdomColor = () => {
+    const color = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    console.log("Color hexadecimal generado: " + color);
+    return color;
+
+}
+
+// Función para cambiar el color de fondo del body
+const changeBackgroundColor = () => {
+    const newColor = generateRamdomColor();
+    document.body.style.backgroundColor = newColor;
+}
+
+// Agrega un evento de clic al botón
+const changeColorBtn = document.getElementById("changeColorBtn");
